91fa6eb5e6c14dec4fa6265cab99938b11ad9c71
alumAle <101581564+alumAle@users.noreply.github.com>
2022-03-24 17:34:33 +0100

    Update usuarios.h

--- usuarios.h
+++ usuarios.h
@@ -1,17 +1,30 @@
-#ifndef _USUARIOS_H_
-#define _USUARIOS_H_
+#include <stdio.h>
+#ifndef USUARIOS
+#define USUARIOS
 
 typedef struct{
-    char id[4];
+    int id;
     char nombre[21];
     char perfil[14];
     char usuario[6];
     char pass[9];
-}Usuarios;
+}Usuario;
 
 
-Usuarios* getUsuarios(int *arraySize);
-void setUsuarios (Usuarios* data, int i);
-void listarUsuarios(Usuarios *data, int arraySize);
+Usuario* getUsuarios(int *arraySize);
+void setUsuarios (Usuario* data, int i);
+
+void menuAdminUsuarios(Usuario** usuarios, int* nUsuarios);
+void listarUsuarios(Usuario *data, int arraySize);
+void imprimeUsuario(Usuario u);
+void addUsuario(Usuario** usuarios, int* nUsuarios);
+void cambiarUsuarioUsuario(int idVec, Usuario** usuarios, int nUsuarios);
+void cambiarPasswordUsuario(int idVec, Usuario** usuarios, int nUsuarios);
+void cambiarNombreCompletoUsuario(int idVec, Usuario** usuarios, int nUsuarios);
+void cambiarPerfilUsuario(int idVec, Usuario** usuarios, int nUsuarios);
+int existeNickUsuario(Usuario** usuarios, int nUsuarios, char* nick, int indice);
+int existeNombreUsuario(Usuario** usuarios, int nUsuarios, char* usuario, int indice);
+void modificarUsuario(Usuario** usuarios, int nUsuarios);
+void eliminarUsuario(Usuario** usuarios, int* nUsuarios);
 
 #endif
