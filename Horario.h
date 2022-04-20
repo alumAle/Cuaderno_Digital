c49c61457e00ab8c27ab4a5f75befc8ac5b5106a
Joseeelv <100365898+Joseeelv@users.noreply.github.com>
2022-04-20 10:41:11 +0200

    Update Horario.h

--- Horario.h
+++ Horario.h
@@ -3,24 +3,24 @@
 #define HORARIOS
 
 typedef struct{
-  char id_prof[3];
-  char dia[5];
-  char hora[6];
-  char id_mat[4];
-  char grupo[10];
+  int id_prof;
+  int dia;
+  int hora;
+  int id_mat;
+  char grupo[11];
 }Horario;
 
-Horario* getHorario(int *arraySize);
-void setHorario(Horario* data, int i);
-void listarhorario(Horario* Horarios, int nHorarios);
-void editarHorario();
-void eliminarHorario();
-void crearHorario();
-
-
-
-
-
+Horario* getHorarios(int *arraySize);
+void setHorarios(Horario* data, int i);
+void crearhorario(Horario** Horarios, int *nHorarios,int id_prof);
+void eliminarHorario(Horario** Horarios, int *nHorarios, int id_prof);
+void modificarHorarioProfe(Horario** Horarios, int nHorarios, int id_prof);
+void imprimeHorario(Horario *h , int );
+void listarHorarioProfe(Horario* Horarios, int nHorarios, int id_prof);
+void cambiarId(int id, Horario** Horarios);
+void cambiarDia(int id, Horario** Horarios);
+void cambiarHora(int id, Horario** Horarios);
+void cambiarMat(int id, Horario** Horarios);
+void cambiarGrupo(int id, Horario** Horarios);
 
 #endif // !HORARIO
-
